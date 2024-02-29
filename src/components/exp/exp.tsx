0e4bde544dba56a4dcb7a007e9a7956e64c789fa
Arthur <goolebyaqa@gmail.com>
2024-03-01 00:05:44 +0500

    feat: add adaptive

--- src/components/exp/exp.tsx
+++ src/components/exp/exp.tsx
@@ -5,12 +5,12 @@ const Exp = () => {
     <div className={expStyles.wrapper}>
       <h2>My study projects:</h2>
       <ul className={expStyles.expList}>
+        <li><button className={expStyles.expLink}><a href="https://qoolebyaqa.github.io/Shelter-VanillaJS/shelter/pages/main/">Shelter</a></button></li>
+        <li><button className={expStyles.expLink}><a href="https://qoolebyaqa.github.io/OnlineZoo-VanillaJS/online-zoo/pages/main/">Online-zoo</a></button></li>
+        <li><button className={expStyles.expLink}><a href="https://qoolebyaqa.github.io/AudioQuiz-JS/start-page/">Audio-Quiz</a></button></li>
+        <li><button className={expStyles.expLink}><a href="https://qoolebyaqa.github.io/game15-VanillaJS/gem-puzzle/">Game-15</a></button></li>
         <li><button className={expStyles.expLink}><a href="https://qoolebyaqa.github.io/online-store/">Online-store</a></button></li>
         <li><button className={expStyles.expLink}><a href="https://qoolebyaqa.github.io/RS-Clone/">Task-board</a></button></li>
-        <li><button className={expStyles.expLink}><a href="https://qoolebyaqa.github.io/AudioQuiz-JS/start-page/">Audio-Quiz</a></button></li>
-        <li><button className={expStyles.expLink}><a href="https://qoolebyaqa.github.io/OnlineZoo-VanillaJS/online-zoo/pages/main/">Online-zoo</a></button></li>
-        <li><button className={expStyles.expLink}><a href="https://qoolebyaqa.github.io/Shelter-VanillaJS/shelter/pages/main/">Shelter</a></button></li>
-        <li><button className={expStyles.expLink}><a href="https://qoolebyaqa.github.io/game15-VanillaJS/gem-puzzle/">Game-15</a></button></li>
       </ul>
     </div>
   )
--- src/components/skills/skills.tsx
+++ src/components/skills/skills.tsx
@@ -4,16 +4,18 @@ import skillsStyles from "./skills.module.scss"
 const Skills = () => {
   return (
     <div className={skillsStyles.skills}>
-      <h2>My Skills:</h2>
-      <ul className={skillsStyles.skillslist}>
-        <li>Git</li>
-        <li>MarkDown</li>
-        <li>HTML</li>
-        <li>CSS - SCSS</li>
-        <li>JavaScript, TypeScript</li>
-        <li>FrameWorks: React, Angular - Beginer</li>
-      </ul>
-      <Education />
+      <div className={skillsStyles.wrapper}>
+        <h2>My Skills:</h2>
+        <ul className={skillsStyles.skillslist}>
+          <li>Git</li>
+          <li>MarkDown</li>
+          <li>HTML</li>
+          <li>CSS - SCSS</li>
+          <li>JavaScript, TypeScript</li>
+          <li>FrameWorks: React, Angular - Beginer</li>
+        </ul>
+      </div>
+    <Education />
     </div>
   )
 }
